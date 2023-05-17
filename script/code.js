c5cdf4961c108212ebf698968b12ea4b5647120d
SmasherK <ksikalan@gmail.com>
2023-05-17 09:34:00 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,14 @@
+let person1 ={
+    pname: "Johanne",
+    age: 22
+}
+console.log(person1);
+let person2 ={
+    pname: "Zack",
+    age: 21
+}
+console.log(person2);
+let arrP1=[person1.pname,person1.age]
+console.log(arrP1);
+let arrP2=[person2.pname,person2.age]
+console.log(arrP2);
